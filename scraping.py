5c62449a98406293808c2bd0271dbd36495bafbc
soy david <davidgomezlala8@gmail.com>
2025-10-25 22:09:31 +0200

    script funcional para mejorar

--- scraping.py
+++ scraping.py
@@ -1,2 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = "https://buscador.masterendaw.es/"
+
+response = requests.get(url)
+html = response.text
+
+soup = BeautifulSoup(html, "html.parser")
+
+valor = soup.find(id= "totalCompanies").text
+print(f"{valor}")
